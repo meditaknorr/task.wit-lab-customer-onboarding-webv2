c1b1273759310d97cfcc0877a28912e279060562
Medita Bola <medita.bola@wit-software.com>
2021-09-28 08:41:20 +0200

    Update themeProvider.js
    Added errorColor, sucessColor, global fontSize to 16

--- src/Configs/Themes/themeProvider.js
+++ src/Configs/Themes/themeProvider.js
@@ -3,9 +3,11 @@ export const themeProvider = {
     highlightColor: '42, 82, 229',
     backgroundColor: 'rgba(255, 255, 255, 1.0)',
     fontFamily: '\'Inter\', sans-serif',
-    fontSize: 20,
+    fontSize: 16,
     fontColor: '69, 69, 69',
     lineColor: '69, 69, 69',
+    errorColor: '230, 0, 0',
+    successColor: '0, 153, 0',
   },
   night: {},
 };
