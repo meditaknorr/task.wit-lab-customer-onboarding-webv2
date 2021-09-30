2ce1f44f5f591592c623eb920f4dae6f1526837f
Telvio Sheldon <tsheldon@mail.wit-software>
2021-09-30 01:04:29 -0800

    Added <ConfirmPhoto> component

---
+++ src/Components/ConfirmPhoto/ConfirmPhoto.jsx
@@ -0,0 +1,93 @@
+import React, { useContext } from 'react';
+import { StateContext, DispatchContext } from '../../Contexts/AppStoreContexts';
+import Header from '../../Components/Header/Header';
+import { useLocale } from '../../Hooks/useLocale';
+import { ConfirmPhotoScreen } from './Style';
+import Button from '../../Components/Button/Button';
+import PropTypes from 'prop-types';
+
+
+/**
+ * Returns a React Component to confirm if the the user wants to use the current picture of not.
+ * @param description  { a String that defines the description of the photo that is ging to be displayed }
+ * @param photo {a String containing the base64 text of the image}
+ * @param takeAnotherPhoto {a FUNCTION to discard the current photo and open the camera again}
+ * @param usePhoto {a FUNCTION that uses the taken photo and sends it to te store}
+ * @param storeProperty {a String containing the key value of the property that needs to be sent to the store}
+ * @param screenLabel {String with the header label for the screen}
+ * @param progressBar {a Number with the percent of the progress bar divided by 10 (must be from 0 - 10)}
+ * @param overlayMode {a Number containing key that indicates either the frame is rounded or squared - 1 for rounded and 0 for squared}
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const ConfirmPhoto = ({ description, photo, usePhoto, takeAnotherPhoto, storeProperty, screenLabel, progressBar, overlayMode }) => {
+
+    const state = useContext(StateContext);
+    const dispatch = useContext(DispatchContext);
+    const language = (state.filter((data) => data.id === 1))[0];
+    const { appString } = useLocale(language.language);
+
+    const useThisPhoto = () => {
+        dispatch({
+            type: 'ADD_STATE',
+            payload: {
+                id: storeProperty.id,
+                [storeProperty.storePropertyName]: photo[storeProperty.storePropertyName]
+            }
+        });
+        usePhoto();
+    };
+
+    return (
+        <>
+            <Header
+                logo={0}
+                backButton={1}
+                screenLabel={screenLabel}
+                languageButton={1}
+                language={language[0].language}
+                languageSetter={languageSetter}
+                progressBarPercent={progressBar}
+            />
+            <ConfirmPhotoScreen overlayMode={overlayMode}>
+                <div className="confirm-photo">
+                    <div className="confirm-photo__description">
+                        <span>{description}</span>
+                    </div>
+                    <div className="confirm-photo__image-wrapper row">
+                        <img className="confirm-photo__image" src={photo[storeProperty.storePropertyName]} alt='The image that was captured in the previous page'/>
+                    </div>
+
+                    <div className="footer">
+                        <Button
+                            text={appString.translations.confirmPhoto.usePhoto}
+                            action={useThisPhoto}
+                            mode="normal"
+                        >
+                        </Button>
+
+                        <Button
+                            text={appString.translations.confirmPhoto.takeAnotherPhoto}
+                            action={takeAnotherPhoto}
+                            mode="flat"
+                        >
+                        </Button>
+                    </div>
+                </div>
+            </ConfirmPhotoScreen>
+        </>
+    );
+};
+
+ConfirmPhoto.propTypes = {
+    description: PropTypes.string.isRequired,
+    photo: PropTypes.object.isRequired,
+    takeAnotherPhoto: PropTypes.func.isRequired,
+    usePhoto: PropTypes.func.isRequired,
+    storeProperty: PropTypes.object.isRequired,
+    screenLabel: PropTypes.string.isRequired,
+    progressBar: PropTypes.number.isRequired,
+    overlayMode: PropTypes.number.isRequired,
+}
+
+export default ConfirmPhoto;
--- src/Locales/translations.js
+++ src/Locales/translations.js
@@ -21,22 +21,31 @@ export const translations = [
       },
     ],
     translations:
-      {
-        header: {
-          phoneNumber: 'phone number',
-        },
-        onboarding: {
-          createYour: 'Create your',
-          witAccount: 'WIT Account',
-          toOpen: 'To open your account, you need to:',
-          validatePhone: 'Validate your phone number',
-          scanDocument: 'Scan your voter card',
-          takeSelfie: 'Take a selfie',
-          buttonStart: 'Let\'s start',
-          alreadyRegstered: 'Already Registered?',
-          seeStatus: 'See status',
-        },
+    {
+      header: {
+        phoneNumber: 'phone number',
       },
+      onboarding: {
+        createYour: 'Create your',
+        witAccount: 'WIT Account',
+        toOpen: 'To open your account, you need to:',
+        validatePhone: 'Validate your phone number',
+        scanDocument: 'Scan your voter card',
+        takeSelfie: 'Take a selfie',
+        buttonStart: 'Let\'s start',
+        alreadyRegstered: 'Already Registered?',
+        seeStatus: 'See status',
+      },
+      confirmPhoto:
+      {
+        voterCard: 'Voter card',
+        frontSidePhoto: 'Photo of the front side of your voter card',
+        backSidePhoto: 'Photo of the back side of your voter card',
+        usePhoto: 'Use this photo',
+        takeAnotherPhoto: 'Take another photo',
+        selie: 'Selfie'
+      },
+    },
   },
   {
     id: 2,
@@ -60,22 +69,30 @@ export const translations = [
       },
     ],
     translations:
-      {
-        header: {
-          phoneNumber: 'número de Telefone',
-        },
-        onboarding: {
-          createYour: 'Crie a sua',
-          witAccount: 'Conta WIT',
-          toOpen: 'Para poder criar a sua conta, você precisa:',
-          validatePhone: 'Validar o seu número de telefone.',
-          scanDocument: 'Digitalizar o seu cartão de eleitor.',
-          takeSelfie: 'Tire ou carregar uma foto sua, selfie.',
-          buttonStart: 'Criar uma conta',
-          alreadyRegstered: 'já se registrou?',
-          seeStatus: 'Verifique o estado',
-        },
+    {
+      header: {
+        phoneNumber: 'número de Telefone',
       },
+      onboarding: {
+        createYour: 'Crie a sua',
+        witAccount: 'Conta WIT',
+        toOpen: 'Para poder criar a sua conta, você precisa:',
+        validatePhone: 'Validar o seu número de telefone.',
+        scanDocument: 'Digitalizar o seu cartão de eleitor.',
+        takeSelfie: 'Tire ou carregar uma foto sua, selfie.',
+        buttonStart: 'Criar uma conta',
+        alreadyRegstered: 'já se registrou?',
+        seeStatus: 'Verifique o estado',
+      },
+      confirmPhoto:
+      {
+        voterCard: 'Cartão de eleitor',
+        frontSidePhoto: 'Foto frontal do seu cartão de eleitor',
+        backSidePhoto: 'Foto do verso do seu cartão de eleitor',
+        usePhoto: 'Usar esta foto',
+        takeAnotherPhoto: 'Tirar outra foto',
+      },
+    },
   },
   {
     id: 3,
@@ -100,21 +117,29 @@ export const translations = [
       },
     ],
     translations:
-      {
-        header: {
-          phoneNumber: 'numero di telefono',
-        },
-        onboarding: {
-          createYour: 'Crea il tuo',
-          witAccount: 'Conto WIT',
-          toOpen: 'Per aprire il tuo account, devi:',
-          validatePhone: 'Convalida il tuo numero di telefono',
-          scanDocument: 'Scansiona la tua tessera elettorale',
-          takeSelfie: 'Fai un selfie',
-          buttonStart: 'Creare un account',
-          alreadyRegstered: 'Già Registrato?',
-          seeStatus: 'Vedi stato',
-        },
+    {
+      header: {
+        phoneNumber: 'numero di telefono',
       },
+      onboarding: {
+        createYour: 'Crea il tuo',
+        witAccount: 'Conto WIT',
+        toOpen: 'Per aprire il tuo account, devi:',
+        validatePhone: 'Convalida il tuo numero di telefono',
+        scanDocument: 'Scansiona la tua tessera elettorale',
+        takeSelfie: 'Fai un selfie',
+        buttonStart: 'Creare un account',
+        alreadyRegstered: 'Già Registrato?',
+        seeStatus: 'Vedi stato',
+      },
+      confirmPhoto:
+      {
+        voterCard: 'Tessera elettorale',
+        frontSidePhoto: 'Foto frontale della tua tessera elettorale',
+        backSidePhoto: 'Foto del retro della tessera elettorale',
+        usePhoto: 'Usa questa foto',
+        takeAnotherPhoto: 'Fai un\'altra foto',
+      },
+    },
   },
 ];
