90756c4a3137042639a19f9b2275cd8cd00b14a4
Medita Bola <medita.bola@wit-software.com>
2021-10-01 12:32:10 +0200

    refactor: Updated store
    Removed old store, and added a new one.

---
+++ src/Store/StoreContexts.jsx
@@ -0,0 +1,6 @@
+import { createContext } from 'react';
+
+const StoreStateContext = createContext();
+const StoreDispatchContext = createContext();
+
+export { StoreStateContext, StoreDispatchContext };
---
+++ src/Store/StoreReducer.js
@@ -0,0 +1,54 @@
+import { whitelist } from '../Locales/whitelist';
+
+/**
+ * Language White List Checker
+ * >> Determines if the detected language
+ * Is available to be used on the app or not
+ * Return detected browser language or the default 'eng' language
+ * @param browserLanguage
+ * @returns string
+ */
+const whitelistChecker = (browserLanguage) => {
+  const result = whitelist.filter((data) => (data === browserLanguage));
+  return (result[0] ? browserLanguage : 'en');
+};
+
+export const initialState = {
+  app: {
+    id: 1,
+    language: whitelistChecker((window.navigator.language).slice(0, 2)),
+    defaultCountry: 'Mozambique',
+    defaultCountryCallingCode: '+258',
+    defaultCountryCode: 258,
+  },
+  user: {
+    id: 2,
+    phoneCallingCode: null,
+    phone: null,
+    personalDetails: [],
+  },
+};
+
+export const reducer = (state, action) => {
+  console.log(action);
+  switch (action.type) {
+    case 'SET_USER':
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          ...action.payload,
+        },
+      };
+    case 'SET_APP':
+      return {
+        ...state,
+        app: {
+          ...state.app,
+          ...action.payload,
+        },
+      };
+    default:
+      return state;
+  }
+};
