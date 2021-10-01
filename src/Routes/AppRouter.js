d262967bb754f9be7ec2f584f93a2cfeab282029
Medita Bola <medita.bola@wit-software.com>
2021-10-01 14:40:38 +0200

    feat: Added Details feature screen page

--- src/Routes/AppRouter.js
+++ src/Routes/AppRouter.js
@@ -3,9 +3,10 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Onboarding from '../Screens/Onboarding/Onboarding';
 import PhoneNumber from '../Screens/Onboarding/PhoneNumber/PhoneNumber';
 import PhoneNumberConfirmation from '../Screens/Onboarding/PhoneNumberConfirmation/PhoneNumberConfirmation';
-import Selfie from '../Screens/Validation/Selfie/Selfie';
-import ScanVoterCardBack from '../Screens/Validation/ScanVoterCard/ScanVoterCardBack';
 import ScanVoterCardFront from '../Screens/Validation/ScanVoterCard/ScanVoterCardFront';
+import Selfie from '../Screens/Validation/Selfie/Selfie';
+import Details from '../Screens/Details/Details';
+import ScanVoterCardBack from '../Screens/Validation/ScanVoterCard/ScanVoterCardBack';
 
 const AppRouter = () => (
   <Router>
@@ -16,6 +17,7 @@ const AppRouter = () => (
       <Route exact path="/registration/validation/scan/front" component={ScanVoterCardFront} />
       <Route exact path="/registration/validation/scan/back" component={ScanVoterCardBack} />
       <Route exact path="/registration/validation/selfie" component={Selfie} />
+      <Route exact path="/registration/details" component={Details} />
     </Switch>
   </Router>
 );
