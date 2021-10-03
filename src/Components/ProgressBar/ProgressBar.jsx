66bf0152f27de40d34cfcbaf628b4bda858d753e
Medita Bola <medita.bola@wit-software.com>
2021-10-03 17:11:14 +0200

    refactor: Updated progress bar rules

--- src/Components/ProgressBar/ProgressBar.jsx
+++ src/Components/ProgressBar/ProgressBar.jsx
@@ -6,46 +6,45 @@ const ProgressBar = () => {
   const location = useLocation();
   const [progressLevel, setProgressLevel] = useState(0);
 
-  const progressLevelHandler = (pathname) => {
-    switch (pathname) {
-      case '/registration/onboarding/':
-        setProgressLevel(0);
-        break;
-      case '/registration/onboarding/phonenumber':
-        setProgressLevel(1);
-        break;
-      case '/registration/onboarding/phonenumber/confirmation':
-        setProgressLevel(2);
-        break;
-      case '/registration/validation/scan/front':
-        setProgressLevel(3);
-        break;
-      case '/registration/validation/scan/back':
-        setProgressLevel(5);
-        break;
-      case '/registration/validation/selfie':
-        setProgressLevel(6);
-        break;
-      case '/registration/details':
-        setProgressLevel(7);
-        break;
-      case '/registration/details/additionalinformation':
-        setProgressLevel(8);
-        break;
-      case ('/registration/details/confirmation'):
-      case ('/registration/details/confirmation/successful/'):
-      case '/registration/details/confirmation/unsuccessful/':
-        setProgressLevel(10);
-        break;
-      default:
-        setProgressLevel(0);
-        break;
-    }
-  };
-
   useEffect(() => {
     if (location.pathname) {
-      progressLevelHandler(location.pathname);
+      switch (location.pathname) {
+        case '/registration/onboarding/':
+          setProgressLevel(0);
+          break;
+        case '/registration/onboarding/phonenumber':
+          setProgressLevel(1);
+          break;
+        case '/registration/onboarding/phonenumber/confirmation':
+          setProgressLevel(2);
+          break;
+        case '/registration/validation':
+          setProgressLevel(3);
+          break;
+        case '/registration/validation/scan/front':
+          setProgressLevel(4);
+          break;
+        case '/registration/validation/scan/back':
+          setProgressLevel(5);
+          break;
+        case '/registration/validation/selfie':
+          setProgressLevel(6);
+          break;
+        case '/registration/details':
+          setProgressLevel(7);
+          break;
+        case '/registration/details/additionalinformation':
+          setProgressLevel(8);
+          break;
+        case ('/registration/details/confirmation'):
+        case ('/registration/details/confirmation/successful/'):
+        case '/registration/details/confirmation/unsuccessful/':
+          setProgressLevel(10);
+          break;
+        default:
+          setProgressLevel(0);
+          break;
+      }
     }
   }, []);
 
