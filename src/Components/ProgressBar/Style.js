590a8961cd068ff1c1b6cdb8208bacc0f5224da6
Medita Bola <medita.bola@wit-software.com>
2021-10-04 16:10:52 +0200

    refactor: Restoring deleted progress bar

---
+++ src/Components/ProgressBar/Style.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+import { ConvertorPixel2Rem } from '../../Hooks/useDimension';
+import { themeProvider } from '../../Configs/Themes/themeProvider';
+
+export const LoadingBar = styled.div`
+  position: relative;
+  width: 100%;
+  height: ${ConvertorPixel2Rem(2)};
+  background-color: ${themeProvider.day.lineColor};
+
+  div.Loading-Signal {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: ${(props) => `${props.percent * 10}%`};
+    height: ${ConvertorPixel2Rem(2)};
+    background-color: ${themeProvider.day.highlightColor};
+  }
+`;
