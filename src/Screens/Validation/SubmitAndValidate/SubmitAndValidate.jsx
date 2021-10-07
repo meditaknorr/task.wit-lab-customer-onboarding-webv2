ec477362bd113fa8f900b10173a89df0cb35eba6
Medita Bola <medita.bola@wit-software.com>
2021-10-07 07:55:37 +0200

    refactor: Added timeOut to Submit and Validate
    TimeOut and Header

--- src/Screens/Validation/SubmitAndValidate/SubmitAndValidate.jsx
+++ src/Screens/Validation/SubmitAndValidate/SubmitAndValidate.jsx
@@ -8,60 +8,70 @@ import validatingImage from '../../../Assets/Images/icons/icon_loading.svg';
 import errorImage from '../../../Assets/Images/icons/illustrations_card_error.svg';
 
 const SubmitAndValidate = () => {
-  const { app, user } = storeGetter();
+  const { app, demo, user } = storeGetter();
   const dispatch = storeSetter();
   const { appString } = useLocale(app.language);
   const router = useHistory();
   const [showHeader, setShowHeader] = useState(false);
   // eslint-disable-next-line max-len
-  const submissionResult = false; // this is a mock value to define if the submission was sucessfully submitted or not
+  const submissionResult = demo.errorOnSubmitAndValidation === user.phone; // this is a mock value to define if the submission was sucessfully submitted or not
   const [submissionStatus, setSubmissionStatus] = useState({
     image: validatingImage,
-    alt: 'Icon that inddicates that the submission is beig validated',
+    alt: 'Icon that indicates that the submission is being validated',
     description: {
       description1: appString.translations.submitAndValidate.validating,
     },
   });
 
-
   const restartTheSubmissionProcess = () => {
-    dispatch({ type: 'REMOVE_STATE', payload: { id: 4 } });
-    dispatch({ type: 'REMOVE_STATE', payload: { id: 5 } });
-    dispatch({ type: 'REMOVE_STATE', payload: { id: 6 } });
+    dispatch({ type: 'REMOVE_MEDIA', payload: { id: 4 } });
+    dispatch({ type: 'REMOVE_MEDIA', payload: { id: 5 } });
+    dispatch({ type: 'REMOVE_MEDIA', payload: { id: 6 } });
 
-    router.push('/registration/validation/scan-voter-card-front');
+    router.push('/registration/validation/scan/front');
+  };
+
+  const setResultOnScreen = () => {
+    setShowHeader(true);
+    setSubmissionStatus({
+      image: errorImage,
+      alt: 'Icon that indicates that an error occurred during the submission',
+      title: appString.translations.submitAndValidate.photoUnclear,
+      description: {
+        description1: appString.translations.submitAndValidate.photoUnclearDescription1,
+        description2: appString.translations.submitAndValidate.photoUnclearDescription2,
+      },
+      buttonText: appString.translations.submitAndValidate.scanVoterID,
+      action: restartTheSubmissionProcess,
+    });
   };
 
   useEffect(() => {
-    if (submissionResult) {
-      router.push('/registration/details/review-details');
-    }
-    else {
-      setShowHeader(true);
-      setSubmissionStatus({
-        image: errorImage,
-        alt: 'Icon that inddicates that an error occured during the submission',
-        title: appString.translations.submitAndValidate.photoUnclear,
-        description: {
-          description1: appString.translations.submitAndValidate.photoUnclearDescription1,
-          description2: appString.translations.submitAndValidate.photoUnclearDescription2,
-        },
-        buttonText: appString.translations.submitAndValidate.scanVoterID,
-        action: restartTheSubmissionProcess,
-      });
-    }
-  }, [submissionResult, appString]);
+    const timer = setTimeout(() => {
+      if (submissionResult) {
+        router.push('/registration/details');
+      } else {
+        setResultOnScreen();
+      }
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, [submissionResult, appString]);// appString
 
   return (
     <>
-      {showHeader && (
+      {showHeader ? (
         <Header
-          logo={0}
-          backButton={1}
+          greyBack={1}
           screenLabel={appString.translations.confirmPhoto.voterCard}
           languageButton={1}
           language={app.language}
-          progressBarPercent={5}
+          progressBar={1}
+          ghostBlock={1}
+        />
+      ) : (
+        <Header
+          language={app.language}
+          ghostBlock={1}
         />
       )}
       <BaseStatusSubmission
