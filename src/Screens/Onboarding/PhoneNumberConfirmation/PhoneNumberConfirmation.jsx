080bd543a5283e4059e67bdb2f70f54f42aa0a22
Medita Bola <medita.bola@wit-software.com>
2021-10-08 12:27:11 +0200

    refactor: Changed from JS Object state to Array State

--- src/Screens/Onboarding/PhoneNumberConfirmation/PhoneNumberConfirmation.jsx
+++ src/Screens/Onboarding/PhoneNumberConfirmation/PhoneNumberConfirmation.jsx
@@ -1,5 +1,10 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React,
+{
+  useContext, useRef, useState,
+}
+  from 'react';
 import { Button } from 'reactstrap';
+import { v4 as uuidv4 } from 'uuid';
 import { StateContext } from '../../../Contexts/AppStoreContexts';
 import Header from '../../../Components/Header/Header';
 import WebView from '../../../Layouts/WebView/WebView';
@@ -7,59 +12,57 @@ import { PhoneNumberConfirmationScreen, Main, Modal } from './Style';
 import { useLocale } from '../../../Hooks/useLocale';
 
 const PhoneNumberConfirmation = () => {
+  const inputRef = useRef([]);
   const state = useContext(StateContext);
   const language = (state.filter((data) => data.id === 1))[0];
   const userData = (state.filter((data) => data.id === 3))[0];
   const { appString } = useLocale(language.language);
-  const [isValidPin, setIsValidPin] = useState(false);
   const [modal, setModal] = useState(false);
-  const [confirmationPin, setConfirmationPin] = useState({
-    one: '',
-    two: '',
-    three: '',
-    four: '',
-  });
-
-  useEffect(() => {
-    setIsValidPin(!(Object.values(confirmationPin).indexOf('') <= -1));
-  }, [confirmationPin]);
-
-  const handle = (e) => {
-    setConfirmationPin((prevState) => ({
-      ...prevState,
-      [e.target.id]: e.target.value,
-    }));
-  };
-
-  const confirmationPinChecker = (e) => {
-    if ((e.target.value).length > 0) {
-      if (e.target.nextSibling) {
-        e.target.nextSibling.focus();
-      }
-      handle(e);
-    } else if ((e.target.value) === '') {
-      if (e.target.previousSibling) {
-        e.target.previousSibling.focus();
-        setConfirmationPin((prevState) => ({
-          ...prevState,
-          [e.target.id]: '',
-        }));
-      } else {
-        setConfirmationPin((prevState) => ({
-          ...prevState,
-          [e.target.id]: '',
-        }));
-      }
-    }
-  };
-
-  const actionButtonHandler = () => {
-    const thePin = (`${confirmationPin.one}${confirmationPin.two}${confirmationPin.three}${confirmationPin.four}`);
-    setModal(thePin !== '1010');
-  };
+  const [pin, setPIN] = useState([undefined, undefined, undefined, undefined]);
+  let items;
 
   const closeModal = () => {
     setModal(false);
+    setPIN([undefined, undefined, undefined, undefined]);
+  };
+
+  const isComplete = (data) => {
+    const ObjEmpty = Object.keys(data).filter((k) => (data[k] === '' || data[k] === undefined || data[k] === null));
+    if (ObjEmpty.length === 0) {
+      return true;
+    }
+    return false;
+  };
+
+  const inputHandler = (e) => {
+    const index = (e.target.id).slice(5);
+    items = pin;
+    items[index] = e.target.value;
+    if (isComplete(items)) {
+      setPIN([...items]);
+    }
+  };
+
+  const focusHandler = (e) => {
+    const index = parseInt((e.target.id).slice(5), 10);
+    if ((e.target.value).length > 0) {
+      if (index < 3) {
+        inputHandler(e);
+        inputRef.current[index].nextSibling.focus();
+        inputRef.current[index].nextSibling.select();
+      } else if (index < 4) {
+        inputRef.current[3].blur();
+      }
+    } else if (index >= 0) {
+      items = pin;
+      items[index] = undefined;
+      setPIN([...items]);
+    }
+  };
+
+  const buttonHandler = () => {
+    const thePIN = pin.join('');
+    setModal(thePIN !== '1010');
   };
 
   return (
@@ -68,22 +71,24 @@ const PhoneNumberConfirmation = () => {
         <Header
           logo={0}
           backButton={1}
-          screenLabel={1}
+          screenLabel=""
           languageButton={1}
           language={language.language}
         />
+
         <Modal status={modal}>
           <div className="Modal__Pane">
             {/* eslint-disable-next-line max-len */}
             {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/control-has-associated-label,jsx-a11y/interactive-supports-focus */}
-            <div className="Modal__Pane-CloseButton" role="button" onClick={closeModal} />
-            <div className="Modal__Pane-Icon" />
-            <div className="Modal__Pane-Information">
+            <div className="Modal__Pane--CloseButton" role="button" onClick={closeModal} />
+            <div className="Modal__Pane--Icon" />
+            <div className="Modal__Pane--Information">
               <h1>{appString.translations.onboarding.codeIncorrect}</h1>
               <h2>{appString.translations.onboarding.tryAgainCode}</h2>
             </div>
           </div>
         </Modal>
+
         <Main>
           <div className="HeadingText">
             <h1>{appString.translations.onboarding.pleaseConfirm}</h1>
@@ -95,61 +100,37 @@ const PhoneNumberConfirmation = () => {
           </div>
 
           <div className="OneTimePin">
-            <input
-              id="one"
-              type="tel"
-              minLength={1}
-              maxLength={1}
-              size={3}
-              value={confirmationPin.one}
-              onChange={confirmationPinChecker}
-              className="OneTimePin__SquarePin"
-              autoComplete="off"
-            />
-            <input
-              id="two"
-              type="tel"
-              minLength={1}
-              maxLength={1}
-              size={1}
-              value={confirmationPin.two}
-              onChange={confirmationPinChecker}
-              className="OneTimePin__SquarePin"
-              autoComplete="off"
-            />
-            <input
-              id="three"
-              type="tel"
-              minLength={1}
-              maxLength={1}
-              size={1}
-              value={confirmationPin.three}
-              onChange={confirmationPinChecker}
-              className="OneTimePin__SquarePin"
-              autoComplete="off"
-            />
-            <input
-              id="four"
-              type="tel"
-              minLength={1}
-              maxLength={1}
-              size={1}
-              value={confirmationPin.four}
-              onChange={confirmationPinChecker}
-              className="OneTimePin__SquarePin"
-              autoComplete="off"
-            />
+            {
+              pin.map((square, index) => (
+                <input
+                  key={uuidv4()}
+                  id={`box__${index}`}
+                  type="tel"
+                  minLength={1}
+                  maxLength={1}
+                  size={1}
+                  tabIndex={0}
+                  value={pin[index]}
+                  onChange={focusHandler}
+                  onBlur={inputHandler}
+                  ref={(element) => { inputRef.current[index] = element; }}
+                  className="OneTimePin__Square"
+                  autoComplete="off"
+                />
+              ))
+            }
           </div>
 
           <div className="ActionButton">
-            <div className="ActionButton-ResendPin">
-              {(isValidPin) ? null : appString.translations.onboarding.resendCode }
+            <div className="ActionButton__ResendPin">
+              {isComplete(pin) && appString.translations.onboarding.resendCode }
             </div>
             <Button
               type="button"
-              onClick={actionButtonHandler}
-              className="ActionButton-ContinueRegistration"
-              disabled={(isValidPin)}
+              tabIndex={0}
+              onClick={buttonHandler}
+              className="ActionButton__ContinueRegistration"
+              disabled={!isComplete(pin)}
             >
               {appString.translations.onboarding.continue}
             </Button>
