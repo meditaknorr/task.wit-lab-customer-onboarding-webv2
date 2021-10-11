ccef5e4c0d7dd1768f63958472ccadd75dd5f942
Medita Bola <medita.bola@wit-software.com>
2021-10-11 08:16:33 +0200

    bugfix: Updating text Regex validaor

--- src/Helpers/inputFormHelper.js
+++ src/Helpers/inputFormHelper.js
@@ -3,48 +3,70 @@ const regEX = {
   number: new RegExp('^[0-9]*$', 'gm'),
   voters: new RegExp('^[0-9-/()]+$', 'g'),
   address: new RegExp('^[A-Za-z0-9.,\\s\u0080-\uFFFF/]+$', 'g'),
-  text: new RegExp('^[A-Za-z\\s\\s\u0080-\uFFFF/]+$', 'g'),
+  text: new RegExp('^[A-Za-z\\s\u0080-\uFFFF/]+$', 'g'),
   email: new RegExp('^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}$', 'i'),
 };
 
-export const dateValidator = (strings) => {
+export const inputStatus = (input) => {
+  if (input === '' || input === null || input === undefined) {
+    return false;
+  }
+  return true;
+};
+
+export const inputDateValidator = (strings) => {
   if (strings.length <= 0) {
     return false;
   }
+
   const current = new Date();
   const currentYear = current.getFullYear();
   const validator = regEX.date;
   const stringYear = strings.slice(6);
   const isAboveSixteen = currentYear - 16;
-
   validator.lastIndex = 0;
-
   if (stringYear >= 1950 && stringYear <= isAboveSixteen) {
     return validator.test(strings);
   }
   return false;
 };
 
-export const numberValidator = (strings) => {
-  const validator = regEX.date;
+export const inputNumberValidator = (strings) => {
+  if (strings.length <= 0) {
+    return false;
+  }
+
+  const validator = regEX.number;
   validator.lastIndex = 0;
   return validator.test(strings);
 };
 
-export const voterValidator = (strings) => {
+export const inputVoterValidator = (strings) => {
+  if (strings.length <= 0) {
+    return false;
+  }
+
   const validator = regEX.voters;
   validator.lastIndex = 0;
   return validator.test(strings);
 };
 
-export const addressValidator = (strings) => {
-  const validator = regEX.date;
+export const inputAddressValidator = (strings) => {
+  if (strings.length <= 0) {
+    return false;
+  }
+
+  const validator = regEX.address;
   const result = validator.test(strings);
   validator.lastIndex = 0;
   return result;
 };
 
-export const textValidator = (strings) => {
+export const inputTextValidator = (strings) => {
+  if (strings.length <= 0) {
+    return false;
+  }
+
   const validator = regEX.text;
   validator.lastIndex = 0;
   return validator.test(strings);
